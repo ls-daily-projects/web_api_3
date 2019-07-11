f14b1a1d7e85a403a0d8968f267778b1345a09c5
Mikis Woodwinter <mikis.woodwinter@gmail.com>
2019-07-11 05:19:41 -0700

    Add validateUserId middleware
    Signed-off-by: Mikis Woodwinter <mikis.woodwinter@gmail.com>

--- rest-api/src/middleware/index.js
+++ rest-api/src/middleware/index.js
@@ -1,4 +1,5 @@
-import { NotFound } from "http-errors"
+import { getUserById } from "../model"
+import { BadRequest } from "http-errors"
 
 export const handle404 = (req, _res, next) => {
     const { method, path } = req
@@ -9,4 +10,18 @@ export const handle404 = (req, _res, next) => {
 export const handle500 = ({ status = 500, name, message }, _req, res, next) => {
     if (res.headersSent) return next()
     res.status(status).json({ name, statusCode: status, message })
+}
+
+export const validateUserId = async (req, res, next) => {
+    const { userId } = req.params
+
+    try {
+        const user = await getUserById(userId)
+        if (!user) throw BadRequest("Invalid user id")
+        req.user = user
+        next()
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
 }
